474e73f539dd40025c1a6c40dcf84bbd48dcff35
Pavel Karpenko <karpenko.p.s@gmail.com>
2022-05-31 05:48:08 +0300

    Update template files

--- local/templates/kplab_don360/footer.php
+++ local/templates/kplab_don360/footer.php
@@ -5,7 +5,10 @@ if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 
 		</div>
 	</div> <!-- .js-animsition -->
-	<?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/js/bootstrap/bootstrap.js');?>
+<!--	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>-->
+	<?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/assets/bootstrap/dist/js/bootstrap.bundle.min.js');?>
+	<?$APPLICATION->AddHeadScript(SITE_TEMPLATE_PATH . '/assets/js/owl.carousel.min.js');?>
+
 	<script type="text/javascript">
 	    $(document).ready(function() {
 	        $('.reviews').owlCarousel({
